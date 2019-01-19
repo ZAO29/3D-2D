ff792171d98c7f50c7919e0fb7ec7e9f77e30ed9
zao29 <nicolsa.bellot@gmail.com>
2019-01-19 23:32:44 +0100

    unity project : SpeedRun

---
+++ Unity Project/SpeedRun/Assets/Script/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    [SerializeField] public float m_speedStep = 0.0001f;
+    float m_startTime = 0;
+    [SerializeField] public float m_updateSpeedTime = 0.01f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+         if(!UpdateSpeed(m_speedStep, KeyCode.RightArrow))
+        {
+            UpdateSpeed(-m_speedStep, KeyCode.LeftArrow);
+        }
+
+
+
+    }
+
+    bool UpdateSpeed(float a_step,KeyCode a_key)
+    {
+        bool rvalue = Input.GetKey(a_key);
+
+        if (Input.GetKeyDown(a_key))
+        {
+            m_startTime = Time.time;
+        }
+        if (rvalue && (Time.time - m_startTime) > m_updateSpeedTime)
+        {
+            m_startTime = Time.time;
+
+            BackGroundHandler parent = transform.GetComponentInParent<BackGroundHandler>();
+
+            if(parent)
+            {
+                parent.speed += a_step;
+            }
+        }
+
+        return rvalue;
+        
+    }
+}
---
+++ Unity Project/SpeedRun/Assets/Script/Scrolling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float modSize = 20;
+    [SerializeField] private float m_Realspeed;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BackGroundHandler parent = transform.GetComponentInParent<BackGroundHandler>();
+        m_Realspeed = speed *parent.speed;
+        float newPos = Mathf.Repeat(Time.time * m_Realspeed, modSize);
+        transform.position = startPos + Vector2.left * newPos;
+	}
+}
