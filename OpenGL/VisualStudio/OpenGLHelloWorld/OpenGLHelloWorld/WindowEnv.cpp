1a11c78ab64df93381d5d35855ab61606ca38476
zao29 <nicolsa.bellot@gmail.com>
2020-04-07 11:25:40 +0200

    OpenGL class WindowEnv

---
+++ OpenGL/VisualStudio/OpenGLHelloWorld/OpenGLHelloWorld/WindowEnv.cpp
@@ -0,0 +1,87 @@
+#include "stdafx.h"
+#include "WindowEnv.h"
+#include <iostream>
+
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+WindowEnv::WindowEnv()
+{
+}
+
+
+WindowEnv::~WindowEnv()
+{
+}
+
+void WindowEnv::init(int32_t width, int32_t height)
+{
+
+	if (nullptr != m_pwindow)
+	{
+		std::cout << __FUNCTION__ << " window alreadey created " << std::endl;
+		glfwTerminate();
+
+		throw std::runtime_error("failed to create window");
+	}
+
+
+	// Init GLFW
+	glfwInit();
+
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	m_pwindow = glfwCreateWindow(width, height, "LearnOpenGL", nullptr, nullptr);
+
+
+
+	if (nullptr == m_pwindow)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+
+		throw std::runtime_error("failed to create window");
+	}
+
+	glfwMakeContextCurrent(m_pwindow);
+
+	initGlew();
+}
+
+void WindowEnv::swapBuffer()
+{
+	glfwSwapBuffers(m_pwindow);
+}
+
+void WindowEnv::destroy()
+{
+}
+
+void WindowEnv::initGlew()
+{
+	int screenWidth, screenHeight;
+	glfwGetFramebufferSize(m_pwindow, &screenWidth, &screenHeight);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		throw std::runtime_error("Failed to initialize GLEW");
+	}
+
+	// Define the viewport dimensions
+	glViewport(0, 0, screenWidth, screenHeight);
+}
---
+++ OpenGL/VisualStudio/OpenGLHelloWorld/OpenGLHelloWorld/WindowEnv.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+
+struct GLFWwindow;
+
+class WindowEnv
+{
+public:
+	WindowEnv();
+	~WindowEnv();
+
+	void init(int32_t width, int32_t height);
+
+	void swapBuffer();
+
+	void destroy();
+
+	void getScreenSize();
+
+	GLFWwindow * get() { return m_pwindow; };
+
+private:
+
+	void initGlew();
+
+	GLFWwindow * m_pwindow = nullptr;
+	int32_t m_windowWidth = 0;
+	int32_t m_windowHeight = 0;
+};
+
