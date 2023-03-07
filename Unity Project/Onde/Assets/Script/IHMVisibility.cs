3edb1c88d4e7c75180a4cad64c28b68da29d6733
zao29 <nicolsa.bellot@gmail.com>
2023-03-07 18:49:23 +0100

    more efficient visibility

--- Unity Project/Onde/Assets/Script/IHMVisibility.cs
+++ Unity Project/Onde/Assets/Script/IHMVisibility.cs
@@ -7,9 +7,24 @@ public class IHMVisibility : MonoBehaviour
     [SerializeField]
     Canvas UI_canvas = null;
     // Start is called before the first frame update
+    [SerializeField] 
+    bool visible = false;
+
+    public bool Visible { get => visible;
+
+        set
+        {
+            visible = value;
+            SetVisibility(visible);
+        }
+    }
+
     void Start()
     {
         Debug.Assert(UI_canvas != null);
+
+        //force l'update de l'etat
+        Visible = Visible;
     }
 
     // Update is called once per frame
@@ -19,7 +34,18 @@ public class IHMVisibility : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.V))
         {
-            UI_canvas.gameObject.SetActive(!UI_canvas.gameObject.activeInHierarchy);
+            Visible = !Visible;
+            
+        }
+    }
+
+
+    void SetVisibility(bool visible)
+    {
+        for (int i = 0; i < UI_canvas.gameObject.transform.childCount; i++)
+        {
+            var child = UI_canvas.gameObject.transform.GetChild(i);
+            child.gameObject.SetActive(visible);
         }
     }
 }
